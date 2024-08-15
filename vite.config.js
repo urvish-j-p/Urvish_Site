938860b7a74656f67e6e79690e6d41fbd71da8c8
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-08-15 21:51:04 +0530

    removed unwanted code

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        manifest: resolve(__dirname, 'manifest.json') 
-      }
-    }
-  }
-})
+});
