08bcbc1b2809b24a374dc6a1a80be1d12bf19dcd
Urvish Prajapati <urvishmeghani111@gmail.com>
2025-02-22 16:04:27 +0530

    cicd test1

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -29,7 +29,7 @@ export const NAVIGATION_LINKS = [
 ];
 
 export const HERO = {
-  name: "Urvish Prajapati",
+  name: "Urvisssh Prajapati",
   Intro: "I'm",
   greet: "Hello there!",
   description:
